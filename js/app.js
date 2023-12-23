c844d64a0a072cec3556eb41e41dfff0bfe778d3
ManliestBen <benjamintmanley@gmail.com>
2023-12-23 00:14:53 -0600

    add functions to generate board cells and board cell elements

--- js/app.js
+++ js/app.js
@@ -1,13 +1,19 @@
 /*-------------------------------- Constants --------------------------------*/
-
+class Cell {
+  constructor(xPos, yPos){
+    this.xPos = xPos
+    this.yPos = yPos
+  }
+}
 
 
 /*---------------------------- Variables (state) ----------------------------*/
 let currentPiece, nextPiece
+let boardCells = []
 
 
 /*------------------------ Cached Element References ------------------------*/
-const board = document.querySelector('#board')
+const boardElement = document.querySelector('#board')
 
 
 /*----------------------------- Event Listeners -----------------------------*/
@@ -28,6 +34,14 @@ document.addEventListener('keydown', (evt) => {
 
 
 /*-------------------------------- Functions --------------------------------*/
+init()
+
+
+function init() {
+  generateBoardCells()
+  console.log(boardCells)
+  generateBoardCellElements()
+}
 
 function checkForRow() {
 }
@@ -42,6 +56,28 @@ function generatePiece() {
 }
 
 function checkForCollision() {
+}
+
+function generateBoardCellElements() {
+  for (let x = 1; x <= 8; x++) {
+    let newColumn = document.createElement('div')
+    for (let y = 16; y >= 1; y--) {
+      let newCellElement = document.createElement('div')
+      newCellElement.id = `X${x}Y${y}`
+      newCellElement.className = 'cell'
+      newColumn.appendChild(newCellElement)
+    }
+    boardElement.appendChild(newColumn)
+  }
+}
+
+function generateBoardCells() {
+  for (let x = 1; x <= 8; x++) {
+    for (let y = 1; y <= 16; y++) {
+      let newCell = new Cell(x, y)
+      boardCells.push(newCell)
+    }
+  }
 }
 
 function rotatePiece() {
